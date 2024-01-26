4404c3f71d3bb05baf6bbfa81c25d15d384d6bda
nicrodriguezval <nicrodriguezval@gmail.com>
2024-01-26 17:45:17 -0500

    Add Arrays component with dynamic number addition

---
+++ src/components/Arrays.svelte
@@ -0,0 +1,15 @@
+<script>
+	let numbers = [1, 2, 3, 4, 5];
+	$: sum = numbers.reduce((t, n) => t + n, 0);
+
+	function addNumber() {
+		numbers.push(numbers.length + 1);
+		numbers = numbers; // this way don't allocate a new array in heap memory
+	}
+</script>
+
+<section>
+	<h3>Arrays component</h3>
+	<p>{numbers.join(', ')} = {sum}</p>
+	<button on:click={addNumber}>Add number</button>
+</section>
