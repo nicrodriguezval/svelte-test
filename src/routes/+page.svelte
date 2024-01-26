196d6da04e6863079f195d9f30d8b88d1ad9206e
nicrodriguezval <nicrodriguezval@gmail.com>
2024-01-25 19:39:41 -0500

    Add components and styles to +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Test</h1>
-<h2>Hello, world!</h2>
+<script>
+	import Nested from '../components/Nested.svelte';
+	import Button from '../components/Button.svelte';
+
+	const name = 'Svelte';
+	const src = 'https://compote.slate.com/images/697b023b-64a5-49a0-8059-27b963453fb1.gif';
+	const alt = 'Homer hidding';
+</script>
+
+<main>
+	<h1>Test</h1>
+	<h2>Hello, {name.toUpperCase()}!</h2>
+	<img {src} {alt} />
+	<Nested />
+	<Button />
+</main>
+
+<style>
+	main {
+		display: grid;
+		place-content: center;
+		min-height: 100vh;
+		text-align: center;
+	}
+
+	h1 {
+		color: red;
+	}
+</style>
